5998e4d845dd68521f376c0fc9a3af4b371db68c
bonnw <pwonghansa@gmail.com>
2019-02-15 11:29:21 -0600

    initialized project with intellij

---
+++ src/main/java/com/burahan/carsdemo/CarsdemoApplication.java
@@ -0,0 +1,16 @@
+package com.burahan.carsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsdemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CarsdemoApplication.class, args);
+    }
+
+}
+
