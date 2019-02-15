ca7a8b92afe4d0bd13675fbce32544b2a68bf99a
bonnw <pwonghansa@gmail.com>
2019-02-15 11:33:42 -0600

    created Cars class

---
+++ src/main/java/com/burahan/carsdemo/Cars.java
@@ -0,0 +1,29 @@
+package com.burahan.carsdemo;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Cars
+{
+    private @Id @GeneratedValue Long id;
+    private int year;
+    private String brand;
+    private String model;
+
+    public Cars()
+    {
+        // default constructor
+    }
+
+    public Cars(int year, String brand, String model)
+    {
+        this.year = year;
+        this.brand = brand;
+        this.model = model;
+    }
+}
