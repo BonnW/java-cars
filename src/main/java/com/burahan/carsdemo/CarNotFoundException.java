3b9e9e5fef8cfe7423c83d5de79b7822d3e86025
bonnw <pwonghansa@gmail.com>
2019-02-15 11:36:41 -0600

    created CarNotFoundException

--- src/main/java/com/burahan/carsdemo/CarNotFoundException.java
+++ src/main/java/com/burahan/carsdemo/CarNotFoundException.java
@@ -1,5 +1,9 @@
 package com.burahan.carsdemo;
 
-public class CarNotFoundException
+public class CarNotFoundException extends RuntimeException
 {
+    public CarNotFoundException(Long id)
+    {
+        super("Could not find Car " + id);
+    }
 }
